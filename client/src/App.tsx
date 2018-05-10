251d091de0869bb02b1fcbbfba7fe029a778375b
maxprais <maxprais@gmail.com>
2018-05-10 18:03:29 +0300

    install app dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 
 class App extends React.Component {
